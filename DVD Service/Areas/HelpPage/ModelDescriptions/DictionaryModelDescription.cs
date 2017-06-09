167287413ebef5ac5054cff1152e4020febe7ec2
pelks <pelks@DESKTOP-0DLRVV0>
2017-06-08 18:47:22 -0700

    Add project files.

---
+++ DVD Service/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DVD_Service.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ DVD Service/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DVD_Service.WebApiApplication" Language="C#" %>
